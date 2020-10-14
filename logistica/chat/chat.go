5e7f1ff77e23c3903279e9be045348a5dfe6040b
the 432 <ignacio.aedo@sansano.usm.cl>
2020-10-14 19:27:39 -0300

    creado el registro vBeta

--- logistica/chat/chat.go
+++ logistica/chat/chat.go
@@ -1,10 +1,43 @@
 package chat
 
 import (
+        "os"
+        "fmt"
+        "log"
+        "time"
         "golang.org/x/net/context"
+        "strings"
 )
 
 type Server struct {
+}
+//se guarda la orden en registro.csv
+func guardarOrden(id string, producto string, valor string, tienda string, destino string, codigo string){
+        tiempoactual := time.Now()
+        timestamp := tiempoactual.Format("02-01-2006 15:04")
+        if stringsCompare("0", prioritario) == 0{
+                tipof := "normal"
+        }
+        if stringsCompare("1", prioritario) == 0{
+                tipof := "prioritario"
+        }
+        if stringsCompare("2", prioritario) == 0{
+                tipof := "retail"
+        }
+
+        orden := timestamp+","+ id+","+tipof +","+ producto+","+ valor+","+tienda +","+destino +","+codigo
+        archivo, err := os.OpenFile("registro.csv", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w := csv.NewWriter(archivo)
+
+	w.Write(orden)
+
+	w.Flush()
+	archivo.Close()
 }
 
 //func (s *Server) SolicitarSeguimiento(ctx context.Context, codigoSeguimiento *Message) (*Message, error) {
@@ -18,7 +51,8 @@ func (s *Server) EnviarOrden(ctx context.Context, orden *Orden) (*Message, error
         msj := Message{
                 Body: cuerpo,
         }
-        //aqui se implementa que se guarde la info recibida de cliente en un archivo
+
+        guardarOrden(orden.GetId(), orden.GetProducto(), orden.GetValor(), orden.GetTienda(), orden.GetDestino(), orden.GetPrioritario(), codigoSeguimiento)
         //tambien se implementan las colas segun el tipo
         return &msj, nil
 }
--- logistica/logistica.go
+++ logistica/logistica.go
@@ -4,11 +4,17 @@ import (
         "fmt"
         "log"
         "net"
-
+        "os"
         "github.com/432i/T1SisDistribuidos/logistica/chat"
         "google.golang.org/grpc"
 )
-
+func crearRegistro(){
+        archivo, err := os.Create("registro.csv")
+        if err != nil{
+                log.Println(err)
+        }
+        defer archivo.Close()
+})
 func main() {
 
         fmt.Println("Go gRPC Beginners Tutorial!")
@@ -17,6 +23,8 @@ func main() {
         if err != nil {
                 log.Fatalf("failed to listen: %v", err)
         }
+        crearRegistro()
+        
 
         s := chat.Server{}
 
@@ -27,4 +35,5 @@ func main() {
         if err := grpcServer.Serve(lis); err != nil {
                 log.Fatalf("failed to serve: %s", err)
         }
+        fmt.Println("holahola")
 }
