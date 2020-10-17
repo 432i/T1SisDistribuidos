4ac9586500a0ef05c7d30983255f822def053c1e
the 432 <aevi1313@gmail.com>
2020-10-17 16:55:31 -0300

    finanzas

--- finanzas/finanzas.go
+++ finanzas/finanzas.go
@@ -30,18 +30,8 @@ func main() {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	q, err := ch.QueueDeclare(
-		"testeoooadspasd", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
 	msgs, err := ch.Consume(
-		q.Name, // queue
+		"TestQueue", // queue
 		"",     // consumer
 		true,   // auto-ack
 		false,  // exclusive
